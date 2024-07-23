6a5e684d5df4d33affbc35429e0cbd56a93230f7
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-07-23 14:11:02 +0530

    Create EXP1.2.cpp
    CDS EXP 1

---
+++ EXP1.2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+//Yaman Hasan Ansari PRN:23070123155
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"Enter a and b"<<endl;
+cin>>a;
+cin>>b;
+int sum1 = a+b;
+cout<<"The sum of a and b is"<<sum1;
+int avg = (a+b)/2;
+cout<<endl<< "The average of a and b is:"<<avg;
+return 0;
+}

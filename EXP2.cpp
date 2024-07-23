5c53d9a1f7b3a292597229fba142aa28ad94e90c
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-07-23 14:16:03 +0530

    Create EXP2.cpp
    CDS EXP

---
+++ EXP2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+// Yaman Hasan Ansari PRN: 23070123155
+using namespace std:
+int main()
+{
+int n;
+cout<<"Enter n"<<endl; 
+cin>>n;
+if (n%2==0){
+cout<<endl<<"even";
+}
+else{
+cout<<endl<<"odd";
+}
+return 0;
+}

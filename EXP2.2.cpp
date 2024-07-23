288eb82a1ea2922f7c9ba6202b699a3903b5ca62
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-07-23 14:27:45 +0530

    Create EXP2.2.cpp

---
+++ EXP2.2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//Yaman Hasan Ansari PRN: 23070123155
+using namespace std;
+int main()
+{
+int year;
+cout<<endl<<"Enter the year";
+cin>>year;
+if (year%400==0 and year%100!=0 or year%4==0){
+Cout<<"LEAP year";
+}
+else{
+cout<<"Non leap year";
+}
+return 0;
+}
